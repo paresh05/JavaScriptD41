8314e57ba999c73809c336bc84b722ea8f958698
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-28 20:56:16 +0530

    Added: Progrom to get the maximum and minimum number in theay

--- MinNMax.js
+++ MinNMax.js
@@ -1,14 +1,14 @@
-arrayOfNumbers=[113,555,885,211,114];
-  let maxNumber = arrayOfNumbers[0];
-  let minNumber = arrayOfNumbers[0];
+arrayOfNumbers = [113, 555, 885, 211, 114];
+let maxNumber = arrayOfNumbers[0];
+let minNumber = arrayOfNumbers[0];
 
-  for (let j = 0; j < arrayOfNumbers.length; j++) {
-    if (maxNumber < arrayOfNumbers[j]) {
-      maxNumber = arrayOfNumbers[j];
-    }
-    if (minNumber > arrayOfNumbers[j]) {
-      minNumber = arrayOfNumbers[j];
-    }
+for (let j = 0; j < arrayOfNumbers.length; j++) {
+  if (maxNumber < arrayOfNumbers[j]) {
+    maxNumber = arrayOfNumbers[j];
   }
-  console.log("Maximum number is: " + maxNumber);
-  console.log("Minimum number is: " + minNumber);
+  if (minNumber > arrayOfNumbers[j]) {
+    minNumber = arrayOfNumbers[j];
+  }
+}
+console.log("Maximum number is: " + maxNumber);
+console.log("Minimum number is: " + minNumber);
